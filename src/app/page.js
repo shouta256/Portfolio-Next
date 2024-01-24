092f158ea9c421274699abbaff91200c3e621c94
Shouta256 <shouta160618@gmail.com>
2024-01-24 16:27:39 +0900

    取得したデータを表示 aboutセクションを追加

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,69 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
+import getWorks from '@/app/api';
 
-export default function Home() {
+export default async function Home() {
+  const works = await getWorks();
+
   return (
-    <div className="mx-5">
-      <h1 className="text-8xl md:text-10xl lg:text-12xl text-center my-20 overflow-hidden whitespace-nowrap tracking-wider">
+    <div className='mx-7'>
+      <h1 className='text-[6vw] text-center my-20 overflow-hidden whitespace-nowrap tracking-wider'>
         Unconventional Normalcy
       </h1>
 
-      <h2>Works</h2>
-      <div className="all-project flex flex-wrap">
-        <div className="h-[20vw] my-5 mx-4 bg-red-500" style={{ width: "calc(33% - 2rem"}}>aaa</div>
-        <div className="h-[20vw] my-5 mx-4 bg-red-500" style={{ width: "calc(33% - 2rem"}}>aaa</div>
-        <div className="h-[20vw] my-5 mx-4 bg-red-500" style={{ width: "calc(33% - 2rem"}}>aaa</div>
-        <div className="w-1/4 h-40 my-5 mx-2 bg-yellow-500">aaa</div>
+      <h2 className='text-2xl font-bold'>Works</h2>
+      <div className='all-project flex flex-wrap justify-between'>
+        {works.contents.map((item, index) => (
+          <div
+            key={item.id}
+            className='my-5 bg-red-500 relative object-contain'
+            style={{ width: 'calc(33% - 1rem)', height: '20vw' }}
+          >
+            <Image
+              className='w-full h-full object-cover'
+              src={item.mainPicture.url}
+              alt={item.title}
+              width={500}
+              height={500}
+            />
+            <div
+              className='absolute py-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-white opacity-0 hover:opacity-100 transition-opacity duration-300'
+              style={{ width: '75%', height: '14vw' }}
+            >
+              <h2 className='text-black text-lg font-bold'>{item.title}</h2>
+              <p className=' pt-3'>{item.tech}</p>
+            </div>
+          </div>
+        ))}
+      </div>
 
-        {/* <Link href="WorkPages/Ricebox.html">
-            <Image src="/me.jpg" alt="RiceboxのWEBサイトの画像" width={300} height={200} />
-            <div className="background">
-              <p>
-                <span className="title">Ricebox</span>
-                <br />
-                Webサイト<br />
-                Next.js×Spline
-              </p>
-            </div>
-        </Link>
-        <Link href="WorkPages/Ricebox.html">
-            <Image src="/me.jpg" alt="RiceboxのWEBサイトの画像" width={300} height={200} />
-            <div className="background">
-              <p>
-                <span className="title">Ricebox</span>
-                <br />
-                Webサイト<br />
-                Next.js×Spline
-              </p>
-            </div>
-        </Link>
-        <Link href="WorkPages/Ricebox.html">
-            <Image src="/me.jpg" alt="RiceboxのWEBサイトの画像" width={300} height={200} />
-            <div className="background">
-              <p>
-                <span className="title">Ricebox</span>
-                <br />
-                Webサイト<br />
-                Next.js×Spline
-              </p>
-            </div>
-        </Link> */}
+      <h2 className=' pt-8 text-2xl font-bold'>About</h2>
+      <div style={{ display: 'flex' }}>
+        <div
+          className='flex rounded-full mt-4'
+          style={{ width: '300px', height: '100px' }}
+        >
+          <Image
+            className='rounded-full object-cover'
+            src='/me.jpg'
+            alt='セルフィー'
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className=' mb-10'>
+          <h3 className='text-lg font-semibold mb-5'>スズキ　ショウタ</h3>
+          <p className=' mr-3 text-base'>
+            2004年生まれ。現役の高専生。2024年9月からアメリカの大学に編入予定。
+            <br />
+            学校で学んだことをアウトプットするため、留学費を貯めるためフリーランスを始めました。
+            <br />
+            学生など関係なく実直にクライアントと向き合い、責任を持って取り組みます。
+            <br />
+            実績を積むため提示価格の６割で承ります。
+          </p>
+          <p className='mt-2'>CONTACT:shouta160618@gmail.com</p>
+        </div>
       </div>
     </div>
   );

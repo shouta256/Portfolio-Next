97a590520569d2766a09f312ed70be96da2efaa3
Shouta256 <shouta160618@gmail.com>
2024-01-29 09:04:23 +0900

    英語版詳細ページの作成

---
+++ src/app/work/English/[id]/page.js
@@ -0,0 +1,40 @@
+import parse from 'html-react-parser';
+import { getDetail, getList } from '../../../libs/client';
+import Image from 'next/image';
+import Link from 'next/link';
+import fff from '@/app/page';
+
+export default async function Work({ params }) {
+  const post = await getDetail(params.id);
+  console.log(post);
+
+  return (
+    <div className='m-4'>
+      <div className='flex justify-between my-5'>
+        <h1 className='  mx-8 text-2xl font-extrabold'>{post.title}</h1>
+        <Link href='../' className='mr-8 text-xl font-light'>
+          Back
+        </Link>
+      </div>
+      <h2 className=' mb-8 mx-2 text-xl text-center font-semibold'>
+        {parse(post.description_eng)}
+      </h2>
+      {post.pictures.map((item, index) => (
+        <div
+          key={item.url}
+          className={` w-1/2 h-auto object-cover my-8 ${
+            index % 2 !== 0 ? 'ml-auto' : ''
+          } ${index % 2 === 0 ? 'mr-auto' : ''}`}
+        >
+          <Image
+            className='w-full h-full object-cover'
+            src={item.url}
+            alt={post.title_eng}
+            width={1000}
+            height={1000}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}

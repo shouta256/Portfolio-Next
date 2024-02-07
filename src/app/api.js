7c284d1fe7a50f2e308fb11be5804b082c851ceb
Shouta256 <shouta160618@gmail.com>
2024-02-07 10:47:20 +0900

    無駄なファイルの削除

--- src/app/api.js
+++
@@ -1,8 +0,0 @@
-'use server';
-import { getList } from '@/libs/client';
-
-export async function getWork() {
-  const works = await getList();
-  console.log(works);
-  return works;
-}
--- src/libs/api.js
+++
@@ -1,3 +0,0 @@
-'use server';
-
-import { client } from '@/libs/client';

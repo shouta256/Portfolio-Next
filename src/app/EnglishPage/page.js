81e4ebb9a9817e4fe3d3053c9fc055ca950e69f2
Shouta256 <shouta160618@gmail.com>
2024-02-24 15:07:29 +0900

    レスポンシブ対応

--- src/app/EnglishPage/page.js
+++ src/app/EnglishPage/page.js
@@ -1,51 +1,39 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getList } from '@/libs/client';
+import { isMobile } from 'react-device-detect';
 
-export default async function EnglishHome() {
+export default async function Home() {
   const works = await getList();
-  console.log('EnglishPageで受け取ったworks:', works);
+  console.log('isMobile', isMobile);
   return (
     <div className='mx-7'>
-      <div className='flex'>
-        <Link href='/' className=' text-xl m-3 flex-row-reverse'>
-          Go to Japanese ver
-        </Link>
-      </div>
-
+      <Link href='/' className='changeLang'>
+        Go to Japanese ver
+      </Link>
       {/* キャッチフレーズ */}
-      <h1 className='text-[6vw] text-center my-20 overflow-hidden whitespace-nowrap tracking-wider'>
-        Unconventional Normalcy
-      </h1>
+      <h1 className='catchPhrase'>Unconventional Normalcy</h1>
       {/* Works */}
-      <h2 className='text-2xl font-bold'>Works</h2>
-      <div className='all-project flex flex-wrap justify-between'>
-        {works.contents.reverse().map((item, index) => (
-          // globals.cssでstyleを適用
-          <div
-            key={item.id}
-            className='work my-5 bg-red-500 relative object-contain'
-          >
-            <Link href={`/workEnglish/${item.id}`}>
+      <h2 className='subtitle'>Works</h2>
+      <div className='all-project flex flex-wrap justify-between mb-11'>
+        {works.contents.reverse().map((item) => (
+          <div key={item.id} className='work my-5 relative object-contain'>
+            <Link href={`/work/${item.id}`}>
               <Image
                 className='w-full h-full object-cover'
                 src={item.mainPicture.url}
-                alt={item.title}
+                alt={item.title_eng}
                 width={500}
                 height={500}
               />
               <div
-                className='background absolute py-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-white opacity-0 hover:opacity-100 transition-opacity duration-300 mb:py-3'
-                style={{ width: '75%', height: '13vw' }}
+                className={`projectAbout ${
+                  isMobile ? 'touchDeviceStyle' : 'nonTouchDeviceStyle'
+                }`}
               >
                 {/* global.cssでstyle適用 */}
-                <h2 className='workTitle text-black text-4xl font-bold mb:text-xl'>
-                  {item.title_eng}
-                </h2>
-                <p
-                  className='workDesc mt-5 text-lg mb:text-sm '
-                  style={{ fontSize: '1.3vw' }}
-                >
+                <h2 className='workTitle'>{item.title_eng}</h2>
+                <p className='workDesc'>
                   {item.kinds_eng}
                   <br />
                   {item.tech}
@@ -56,19 +44,18 @@ export default async function EnglishHome() {
         ))}
       </div>
       {/* アバウト */}
-      <h2 className=' pt-8 text-2xl font-bold'>About</h2>
-      <div className='mt-8' style={{ display: 'flex' }}>
+      <h2 className='subtitle'>About</h2>
+      <div className='' style={{ display: 'flex' }}>
         <Image
-          className='rounded-full object-cover'
-          style={{ width: '100px', height: '100px' }}
+          className='icon rounded-full object-cover'
           src='/me.jpg'
           alt='セルフィー'
           width={100}
           height={100}
         />
-        <div className=' ml-10 mb-10'>
-          <h3 className='text-lg font-semibold mb-5'>Shota Suzuki</h3>
-          <p className=' mr-3 text-base'>
+        <div className='aboutDescContainer'>
+          <h3 className='myName'>スズキ　ショウタ</h3>
+          <p className='myDesc'>
             A Japanese individual born in 2004, hailing from Japan, attended a
             National Institute of Technology from the age of 15 to 19.
             <br /> During this time, they studied the fundamentals of computer
@@ -79,7 +66,7 @@ export default async function EnglishHome() {
             received recognition, achieving a Top 100 award among 5000
             submissions.
           </p>
-          <p className='mt-2'>CONTACT:shouta160618@gmail.com</p>
+          <p className='myContact'>CONTACT:shouta160618@gmail.com</p>
         </div>
       </div>
     </div>
